24748b9566ecba80a36e44de3c2508b45337414c
Gabriel Eger <52424334+GabrielEger2@users.noreply.github.com>
2023-07-29 16:54:36 -0300

    feat: Add vertical navbar with Bootstrap icons and mobile support
    Integrated a sleek vertical navbar into the project.
    
    Utilized Bootstrap icons for a visually appealing style.
    
    Implemented JavaScript for a smooth transition to a mobile-friendly navbar on smaller screens.

---
+++ script.js
@@ -0,0 +1,21 @@
+const menuMobile = document.querySelector(".menu-mobile");
+const body = document.querySelector("body");
+menuMobile.addEventListener("click", () => {
+  menuMobile.classList.contains("bi-list-ul")
+    ? menuMobile.classList.replace("bi-list-ul", "bi-x")
+    : menuMobile.classList.replace("bi-x", "bi-list-ul");
+  body.classList.toggle("menu-nav-active");
+});
+
+/ close mobile menu when a section is selected*/
+
+const navItem = document.querySelectorAll('.nav-item')
+
+navItem.forEach(item => {
+  item.addEventListener("click", () => {
+    if (body.classList.contains("menu-nav-active")) {
+      body.classList.remove("menu-nav-active")
+      menuMobile.classList.replace("bi-x", "bi-list-ul");
+    }
+  })
+})
